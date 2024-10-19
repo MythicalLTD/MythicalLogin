fe2b8d5cd9a535fa133df4f5de6bbe865405fd66
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2024-10-19 17:27:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# MythicalLogin
+ MythicalLogin is a login plugin to link your users discord with minecraft!
---
+++ src/main/java/xyz/mythicalsystems/mythicallogin/MinecraftPlugin.java
@@ -0,0 +1,16 @@
+package xyz.mythicalsystems.mythicallogin;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class MinecraftPlugin extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
